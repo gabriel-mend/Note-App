bb24f9a146f2a13ee564739c2ce7d20de8de1769
Gabriel <gabriel@Gabriel-Mac-mini.local>
2020-07-14 18:58:36 -0300

    Backend finalizado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const noteController = require('./controllers/noteController');
 
 const routes = Router();
 
+routes.get('/', noteController.index);
 routes.post('/', noteController.create);
 
 module.exports = routes;
